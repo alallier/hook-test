f712ddbea1f8afe6b31badeb1bbbf9abd0c457b0
Alexander <alexanderlallier@aol.com>
2019-05-15 13:59:19 -0400

    Changes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
+var fs = require('fs')
+
 console.log(process.argv)
 
 console.log('Pre-commit hook running')
+
+var results = fs.readFileSync(process.argv[2])
+
+console.log(results.toString())
+
 process.exit(1)
